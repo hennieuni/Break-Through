e61d96cc2ffbb32e7c3286ea2864620bbb309c37
hennieuni <hennieuni@gmail.com>
2024-10-21 20:44:06 +1300

    spawn change
    setup spawn so after play you are in forest for lec

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    string playerPosFile;
     public void PlayGame(){
-        SceneManager.LoadScene(4);
+        
+
+        playerPosFile = Application.dataPath + "/Saves/PlayerPos.txt";
+        if (!File.Exists(playerPosFile)){
+            File.WriteAllText(playerPosFile, "-9.499884,-1.5");
+        }else{
+            using (StreamWriter writer = new StreamWriter(playerPosFile,false)){
+            writer.WriteLine("-9.499884,-0.5");
+            }
+        }
+        SceneManager.LoadScene(1);
+        
     }
    
     public void QuitGame(){
